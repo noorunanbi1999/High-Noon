e7a10529d2536d8116baa7862b97c39fdb68865f
Syed Mohammed Noorun Nabi <syed.nabi@sonata-software.com>
2024-09-12 11:56:25 +0530

    Started The High Noon project

---
+++ high_noon/src/components/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <div 
+      style={{border:"solid",display:"flex",justifyContent:"space-between"}}
+      >
+<p>About</p>
+<p>Home</p>
+
+
+      </div>
+    </>
+  );
+};
+
+export default Header;
