0ab2d6938f649f70765f6ab21c935d9f5ac4333d
Syed Mohammed Noorun Nabi <syed.nabi@sonata-software.com>
2024-09-12 17:22:26 +0530

    Header has been made

--- high_noon/src/components/Header.js
+++ high_noon/src/components/Header.js
@@ -3,13 +3,49 @@ import React from "react";
 const Header = () => {
   return (
     <>
-      <div 
-      style={{border:"solid",display:"flex",justifyContent:"space-between"}}
+      <div
+        style={{
+          border: "solid",
+          display: "flex",
+          justifyContent: "space-between",
+          padding: "5px 10px",
+          fontSize: "15px",
+          alignItems: "center",
+        }}
       >
-<p>About</p>
-<p>Home</p>
+        <p>About</p>
+        <p>Home</p>
+        <p>About</p>
+        <p>Explore</p>
+        <button
+          style={{
+            backgroundColor: "black",
+            color: "white",
+            border: "none",
+            borderRadius: "22px",
+            paddingLeft: "2%",
+            paddingRight: "2%",
+            height: "24px",
+            
+          }}
+        >
+          search
+        </button>
+        <button
+          style={{
+            backgroundColor: "orange",
+            color: "white",
+            border: "none",
+            borderRadius: "22px",
+            paddingLeft: "2%",
+            paddingRight: "2%",
+            height: "24px",
+          }}
+        >
+          sign-in
+        </button>
 
-
+        <p></p>
       </div>
     </>
   );
