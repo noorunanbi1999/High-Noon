04385483f2895a504f50ea08fe95b2e7b6ec11c6
Syed Mohammed Noorun Nabi <syed.nabi@sonata-software.com>
2024-09-13 16:34:34 +0530

    Body component was added

---
+++ high_noon/src/components/Body.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Body = () => {
+  return (
+    <div //main body div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        border: "solid",
+        justifyContent: "center",
+        background:"black",
+        color:"white",
+      }}
+    >
+      This is where the body belongs
+    </div>
+  );
+};
+
+export default Body;
--- high_noon/src/components/Header.js
+++ high_noon/src/components/Header.js
@@ -1,8 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Header = () => {
+  const [isHoverd, setIsHovered] = useState(false);
+  const handleMouseEnter = () => setIsHovered(true);
+  const handleMouseLeave = () => setIsHovered(false);
+
+  const [isZoomed, setIsZoomed] = useState(false);
+  const handleMouseEnterSearch = () => setIsZoomed(true);
+  const handleMouseLeaveSearch = () => setIsZoomed(false);
+
+  const buttonStyleSearch = {
+    backgroundColor: "black",
+    color: "white",
+    border: "none",
+    borderRadius: "22px",
+    paddingLeft: "2%",
+    paddingRight: "2%",
+    height: "24px",
+    width:"100px",
+    border: "solid",
+    borderBlockColor: "white",
+    
+  };
+
+  const buttonStyleSignIn = {
+    backgroundColor: isHoverd ? "red" : "orange",
+    color: "white",
+    border: "none",
+    borderRadius: "22px",
+    height: "24px",
+    width: "100px",
+  };
+
   return (
-    <>
+    <div>
       <div
         style={{
           border: "solid",
@@ -11,43 +42,38 @@ const Header = () => {
           padding: "5px 10px",
           fontSize: "15px",
           alignItems: "center",
+          backgroundColor: "black",
+          color: "white",
+          paddingLeft: "40px",
+          paddingRight: "40px",
         }}
       >
-        <p>About</p>
-        <p>Home</p>
-        <p>About</p>
-        <p>Explore</p>
-        <button
-          style={{
-            backgroundColor: "black",
-            color: "white",
-            border: "none",
-            borderRadius: "22px",
-            paddingLeft: "2%",
-            paddingRight: "2%",
-            height: "24px",
-            
-          }}
-        >
-          search
-        </button>
-        <button
-          style={{
-            backgroundColor: "orange",
-            color: "white",
-            border: "none",
-            borderRadius: "22px",
-            paddingLeft: "2%",
-            paddingRight: "2%",
-            height: "24px",
-          }}
-        >
-          sign-in
-        </button>
+        <div>
+          <p>High Noon Site For Development</p>
+        </div>
 
-        <p></p>
+        <div
+          style={{
+            display: "flex",
+            // justifyContent: "space-between",
+            gap: "30px",
+            alignItems: "center",
+          }}
+        >
+          <p>About</p>
+          <p>Home</p>
+          <p>Explore</p>
+          <button style={buttonStyleSearch}>search</button>
+          <button
+            style={buttonStyleSignIn}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+          >
+            sign-in
+          </button>
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 
